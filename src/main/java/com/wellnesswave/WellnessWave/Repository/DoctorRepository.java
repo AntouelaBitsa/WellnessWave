35089a75ad1a4b8e32e054560cec67d0de3660b6
AntouelaBitsa <antoubi10@gmail.com>
2024-05-16 11:51:37 +0300

    Adding custom method in DoctorRepository
    Adding custom method in DoctorRepository to find a user by username and password, used for logIN endpoint

--- src/main/java/com/wellnesswave/WellnessWave/Repository/DoctorRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/DoctorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DoctorRepository extends  JpaRepository<Doctor, Long>{
-//    CrudRepository<Doctor, Long>
-   // boolean findDocByEmail(String email);
+public interface DoctorRepository extends JpaRepository<Doctor, Integer>{
+    Doctor findByDocUsernameAndPassword(String docUsername, String password);
+//   boolean findDocByEmail(String email);
 }
