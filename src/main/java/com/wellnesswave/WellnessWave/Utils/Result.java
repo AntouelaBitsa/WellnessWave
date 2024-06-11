bb2d0ce8ad52305210ca5aa613cb6ed945cdcfd8
AntouelaBitsa <antoubi10@gmail.com>
2024-06-11 22:29:08 +0300

    Result Update
    Updating results with necessary methods

--- src/main/java/com/wellnesswave/WellnessWave/Utils/Result.java
+++ src/main/java/com/wellnesswave/WellnessWave/Utils/Result.java
@@ -5,14 +5,26 @@ public class Result {
     private String message;
     private UserSession userSession;
 
+    public Result() {
+    }
+
     public Result(int status, String message) {
         this.status = status;
         this.message = message;
+        System.out.println("1st Constructor");
+        System.out.println("PRINT MESSAGE : " + message);
     }
 
     public Result(int status, UserSession userSession) {
+        //THIS WAS A SECOND CONSTRUCTOR
+//        Result r = new Result(); //TEST
         this.status = status;
         this.userSession = userSession;
+        System.out.println("2nd Constructor USER-SESSION");
+//        r.setStatus(this.status); //TEST
+//        r.setUserSession(this.userSession);  //TEST
+//        System.out.println("r object afte CHANGES" + r.toString());  //TEST
+//        return r;  //TEST
     }
 
     public int getStatus() {
@@ -21,5 +33,25 @@ public class Result {
 
     public String getMessage() {
         return message;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+//    public String getUserSession() {
+//        return userSession;
+//    }
+//
+//    public void setUserSession(String userSession) {
+//        this.userSession = userSession;
+//    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "status=" + status +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
