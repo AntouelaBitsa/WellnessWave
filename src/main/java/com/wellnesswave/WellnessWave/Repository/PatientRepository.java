5b077e5c550a82d86d9df463c105595df6a70035
AntouelaBitsa <antoubi10@gmail.com>
2024-06-11 22:23:44 +0300

    Patient Repository Update
    Adding custom method findByPatUsernameAndPatPassword for the implementation of the query from jpa repository

--- src/main/java/com/wellnesswave/WellnessWave/Repository/PatientRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/PatientRepository.java
@@ -4,4 +4,5 @@ import com.wellnesswave.WellnessWave.Entities.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
+    Patient findByPatUsernameAndPatPassword(String patUsername, String patPassword);
 }
