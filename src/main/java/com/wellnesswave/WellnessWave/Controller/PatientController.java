3a3d2451f3f71bf7d0bcf39eb686b37bb336b25c
AntouelaBitsa <antoubi10@gmail.com>
2024-06-11 22:16:59 +0300

    Patient Controller Updates
    Adding and implementing the endpoints : createPatient
    Adding comments to previous code

--- src/main/java/com/wellnesswave/WellnessWave/Controller/PatientController.java
+++ src/main/java/com/wellnesswave/WellnessWave/Controller/PatientController.java
@@ -2,7 +2,10 @@ package com.wellnesswave.WellnessWave.Controller;
 
 import com.wellnesswave.WellnessWave.Entities.Patient;
 import com.wellnesswave.WellnessWave.Service.PatientService;
+import com.wellnesswave.WellnessWave.Utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,18 +21,40 @@ public class PatientController {
         return patientService.getAllPatients();
     }
 
-    @GetMapping("/getPatientById")
+    @GetMapping("/getPatientById/{id}")
     public Patient getPatientById(@PathVariable Integer id){
         return patientService.getPatientById(id);
     }
 
-    @PostMapping("/updatePatient")
-    public Patient updatePatient(@PathVariable Patient patient){
-        return patientService.updatePatient(patient);
+    //DONE: createPatient endpoint
+    @PostMapping("/createPatient")
+    public ResponseEntity<Result> createPatient(@RequestBody Patient patient){
+        //Start Testing Print
+        System.out.println("//** Test Print : Controller **//");
+        System.out.println(patient.toString());
+        //End of Testing Print
+        return new ResponseEntity<Result>(patientService.createPatient(patient), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/deletePatient")
-    public void deletePatient(@PathVariable Integer id){
-        patientService.deletePatient(id);
-    }
+    //DONE: logIn endpoint
+//    @PostMapping("/patientLogInSession")
+//    public Result logIn(@RequestParam("username") String username, @RequestParam("password") String password){
+//        patientService.logIN(username, password);
+//        if(username.equals(null) && password.equals(null)){
+//            System.out.println("Failed to find patient");
+//            return new Result(1, " Failed to find patient");
+//        }
+//        System.out.println("User Session info in JSON Format");
+//        return new Result(0, "User Session info in JSON Format");
+//    }
+
+//    @PostMapping("/updatePatient")
+//    public Patient updatePatient(@PathVariable Patient patient){
+//        return patientService.updatePatient(patient);
+//    }
+//
+//    @DeleteMapping("/deletePatient")
+//    public void deletePatient(@PathVariable Integer id){
+//        patientService.deletePatient(id);
+//    }
 }
