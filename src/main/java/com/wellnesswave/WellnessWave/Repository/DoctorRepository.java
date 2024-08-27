8c51cddcf1ab2599bc10be02bb7ded80c84adc23
AntouelaBitsa <antoubi10@gmail.com>
2024-08-27 21:27:11 +0300

    Updating Doctor Repository Inteface Class
    Adding method declaration to find user based on profession

--- src/main/java/com/wellnesswave/WellnessWave/Repository/DoctorRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/DoctorRepository.java
@@ -4,8 +4,12 @@ import com.wellnesswave.WellnessWave.Entities.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Integer>{
     Doctor findByDocUsernameAndPassword(String docUsername, String password);
+
+    ArrayList<Doctor> findByProfession(String spec);
 //   boolean findDocByEmail(String email);
 }
