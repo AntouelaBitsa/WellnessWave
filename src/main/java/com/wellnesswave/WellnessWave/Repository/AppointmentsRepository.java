74db81d64cc76a92f8b4d8a75ceee870b890941d
AntouelaBitsa <antoubi10@gmail.com>
2024-11-26 20:11:15 +0200

    Custom query methods to get the neccessary data

--- src/main/java/com/wellnesswave/WellnessWave/Repository/AppointmentsRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/AppointmentsRepository.java
@@ -3,6 +3,10 @@ package com.wellnesswave.WellnessWave.Repository;
 import com.wellnesswave.WellnessWave.Entities.Appointments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentsRepository extends JpaRepository<Appointments, Integer> {
-//    void findByProfession(String spec);
+    List<Appointments> findByPatient_PatientId(Integer patientId);
+    List<Appointments> findByDoctor_DocId(Integer docId);
+    Appointments findByAppointmentId(int appointmentId);
 }
