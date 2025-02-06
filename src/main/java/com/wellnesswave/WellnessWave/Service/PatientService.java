515d826f2ff9cc9c3e74556217bfd011d5e1dcdf
AntouelaBitsa <antoubi10@gmail.com>
2025-02-06 22:38:44 +0200

    Updating Patient Service Class on new methods
    Crearing methods updatePatient, deletePatient by implementing the buisiness logic of those functionalities and updating the logic on method getPatientById to throw RunTimeExceptions in the case the user is not found.

--- src/main/java/com/wellnesswave/WellnessWave/Service/PatientService.java
+++ src/main/java/com/wellnesswave/WellnessWave/Service/PatientService.java
@@ -1,13 +1,22 @@
 package com.wellnesswave.WellnessWave.Service;
 
+import com.wellnesswave.WellnessWave.Entities.Appointments;
+import com.wellnesswave.WellnessWave.Entities.Doctor;
 import com.wellnesswave.WellnessWave.Entities.Patient;
 import com.wellnesswave.WellnessWave.Repository.PatientRepository;
 import com.wellnesswave.WellnessWave.Utils.Result;
 import com.wellnesswave.WellnessWave.Utils.UserSession;
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.logging.log4j.util.Supplier;
+import org.aspectj.util.Reflection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class PatientService {
@@ -20,7 +29,7 @@ public class PatientService {
     }
 
     public Patient getPatientById(Integer id){
-        return patientRep.findById(id).orElse(null);
+        return patientRep.findById(id).orElseThrow(()-> new RuntimeException("Patient Not Found."));
     }
     //DONE: createPatient
     public Result createPatient(Patient patient) {
@@ -53,6 +62,67 @@ public class PatientService {
         return new Result(0, userSession.getPatSessionJSON());
     }
 
+    public Patient updatePatient(Integer id, Map<String, Object> updatedPat){
+        Patient mappedPatientById = this.getPatientById(id);
+        System.out.println("[Debug Message: Update Info] Mapped Patient Id: " + mappedPatientById);
+
+        if (mappedPatientById.hasEmptyOrNull() || mappedPatientById == null){
+            System.out.println("Something went wrong in patient retrieval, some field is null or the patient doesn't exists");
+        }
+
+        updatedPat.forEach((key, value) -> {
+            Field fieldToUpdate = ReflectionUtils.findField(Patient.class, key);
+            if (fieldToUpdate == null){
+                System.out.println("The Filed: " + key + " & in empty or null");
+            }
+            fieldToUpdate.setAccessible(true);
+//            ReflectionUtils.setField(fieldToUpdate, mappedPatientById, value);
+
+            // Check if the field type matches the value type
+            if (fieldToUpdate.getType().isAssignableFrom(value.getClass())) {
+                ReflectionUtils.setField(fieldToUpdate, mappedPatientById, value);
+            } else {
+                System.out.println("Field to Update type" + fieldToUpdate.getType().isAssignableFrom(value.getClass()));
+                System.out.println("Type mismatch: " + key.toString() + " expects " + fieldToUpdate.getType() + " but got " + value.getClass());
+            }
+        });
+
+        return patientRep.save(mappedPatientById);
+    }
+
+//    public boolean deletePatient2(Integer id){
+//        if(id.equals(null)) {
+//            return false;
+//        }
+//        Patient patientToDel = this.getPatientById(id);
+//        System.out.println("PatientToDelete: " + patientToDel);
+//        patientToDel.setAccountStatus("Inactive");
+//        System.out.println("PatientToDelete Status: " + patientToDel.getAccountStatus());
+//        //TODO: Check id
+//        try{
+//            patientRep.save(patientToDel);
+//            System.out.println("PatientToDelete Inside TRY: " + patientRep.save(patientToDel));
+//        }catch (Exception e){
+//            System.out.println(">>> Exception on soft delete : " + e.getMessage());
+//            return false;
+//        }
+//
+//        return true;
+//    }
+
+    public void deletePatient(Integer id){
+        Patient patientToDelete = patientRep.findById(id).orElseThrow(() -> new EntityNotFoundException("Patient Not Found"));
+
+        try {
+            System.out.println("[1 - On Pat Delete] Inside try before Delete: " + patientToDelete);
+            patientRep.deleteById(id);
+//            System.out.println("[2 - On Doc Delete] Inside try before Delete: " + doctorRep.findById(id).orElseThrow(() -> new EntityNotFoundException("Doctor Not Found")));
+        }catch (Exception e){
+            System.out.println("[3 - On Pat Delete] Exception Occurred: " + e.getMessage());
+        }
+
+    }
+
 //    public Patient getPatByAmka(String amka){
 //        Patient p;
 //
@@ -65,13 +135,5 @@ public class PatientService {
 //        }
 //
 //        return p;
-//    }
-
-//    public Patient updatePatient(Patient patient){
-//        return patientRep.save(patient);
-//    }
-//
-//    public void deletePatient(Integer id){
-//        patientRep.deleteById(id);
 //    }
 }
