1f89f6c515d54e8afaa82d2d01a1d35d71058997
AntouelaBitsa <antoubi10@gmail.com>
2025-02-06 22:30:29 +0200

    Update the Diagnosis Repository class by declaring new method
    The new method corresponds to custom quries supported by JPA. this methods returns a list of Diagnosis records for the corresponding patient searched by its ID.

--- src/main/java/com/wellnesswave/WellnessWave/Repository/DiagnosisRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/DiagnosisRepository.java
@@ -3,5 +3,8 @@ package com.wellnesswave.WellnessWave.Repository;
 import com.wellnesswave.WellnessWave.Entities.Diagnosis;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DiagnosisRepository extends JpaRepository<Diagnosis, Integer> {
+    List<Diagnosis> findByPatient_PatientId(Integer patId);
 }
