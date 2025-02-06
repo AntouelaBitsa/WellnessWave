be1559a105e4956668852d7e5a56c158d06e840d
AntouelaBitsa <antoubi10@gmail.com>
2025-02-06 22:36:13 +0200

    Updates on Appointment Repository class by declaring new methods.
    The new methods correspond to custom quries supported by JPA and returns a list of Appointments records for the corresponding Doctor or Patient searched by its ID and the provided date.

--- src/main/java/com/wellnesswave/WellnessWave/Repository/AppointmentsRepository.java
+++ src/main/java/com/wellnesswave/WellnessWave/Repository/AppointmentsRepository.java
@@ -3,10 +3,14 @@ package com.wellnesswave.WellnessWave.Repository;
 import com.wellnesswave.WellnessWave.Entities.Appointments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AppointmentsRepository extends JpaRepository<Appointments, Integer> {
     List<Appointments> findByPatient_PatientId(Integer patientId);
     List<Appointments> findByDoctor_DocId(Integer docId);
     Appointments findByAppointmentId(int appointmentId);
+    List<Appointments> findByDateAndDoctor_DocId(LocalDate date, Integer userId);
+    List<Appointments> findByDateAndPatient_PatientId(LocalDate dateFormatted, Integer userId);
+
 }
